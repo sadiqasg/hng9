ee3895946b9fc8de0cb1790515bbf3fe5ec55432
saslamp <sadiqasg@gmail.com>
2022-10-28 10:06:18 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
-// your code
+app.use(cors({ credentials: true, origin: "*" }));
 
 app.get("/", (req, res) => {
     res.status(200).json({
