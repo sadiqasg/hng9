c5558f24f101caa8756205197b8d81682beeef47
saslamp <sadiqasg@gmail.com>
2022-11-03 16:34:36 +0100

    added files for stage 2 backend task

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
+const { postService } = require("./services");
 
 app.use(cors({ credentials: true, origin: "*" }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -12,6 +14,15 @@ app.get("/", (req, res) => {
         age: 32,
         bio: "Hello! I am a Backend Developer",
     });
+});
+
+app.post("/stage2", (req, res) => {
+    try {
+        postService(req, res);
+    } catch (error) {
+        console.log(error)
+        res.status(400).json("Error");
+    }
 });
 
 app.listen(PORT, () => {
---
+++ services/index.js
@@ -0,0 +1,65 @@
+const postService = (req, res) => {
+    const operation_type = req.body.operation_type;
+
+    const checkSubStr = (string, substing) => {
+        return string.indexOf(substing) !== -1 ? true : false;
+    };
+
+    const operation =
+        checkSubStr(operation_type, "add") ||
+        checkSubStr(operation_type, "addition")
+            ? "addition"
+            : checkSubStr(operation_type, "subtraction") ||
+              checkSubStr(operation_type, "substract")
+            ? "subtraction"
+            : checkSubStr(operation_type, "multiplication") ||
+              checkSubStr(operation_type, "multiply")
+            ? "multiplication"
+            : null;
+
+    if (!operation)
+        return res
+            .status(400)
+            .json(
+                "Please put correct operation - either addition, subtraction, or multiplication"
+            );
+
+    const operation_string = operation_type.split(" ");
+    const numbArr = [];
+    for (let i = 0; i < operation_string.length; i++) {
+        if (parseInt(operation_string[i])) {
+            numbArr.push(operation_string[i]);
+        }
+    }
+
+    const tempX = numbArr.length ? parseInt(numbArr[0]) : null;
+    const tempY = numbArr.length ? parseInt(numbArr[1]) : null;
+
+    const x = req.body.x ? req.body.x : tempX ? tempX : null;
+    const y = req.body.y ? req.body.y : tempY ? tempY : null;
+
+    if (!x || !y)
+        return res.status(400).json("Please add integer values for X and Y");
+
+    const result =
+        operation === "addition"
+            ? x + y
+            : operation === "subtraction"
+            ? x - y
+            : operation === "multiplication"
+            ? x * y
+            : null;
+
+    if (result) {
+        return res.status(200).json({
+            slackUsername: "codeHashira",
+            result: result,
+            operation_type: operation,
+        });
+    }
+    res.status(400).json(
+        "Please make sure you are inputing correct values before I carry cane"
+    );
+};
+
+module.exports = { postService };
